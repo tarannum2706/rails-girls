12f7be0bcf8755c644456860ce07f0ca71c8b430
Tarannum <tanu270011@gmail.com>
2022-04-26 23:10:38 +0530

    initial commit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [ :show, :edit, :update, :destroy ]
-  before_action :authenticate_user!, except:[:index,:show]
+  before_action :authenticate_user!, except:[:index]
 
   # GET /ideas or /ideas.json
   def index
